e10b4f5f1c1c0f8189fe5ca40001f3417be893d1
LAKMAL <manojlakmal999@gmail.com>
2023-04-06 20:25:24 +0530

    adada

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "https://riogamers.lk",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
