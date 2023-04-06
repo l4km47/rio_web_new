dcf2813c9d58ef05d991d4394cfdb54917f4b217
LAKMAL <manojlakmal999@gmail.com>
2023-04-06 19:02:02 +0530

    asd

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
